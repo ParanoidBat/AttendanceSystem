62c770f45856284e480c1f7a5ed0795bd4e7605d
omar azaz <cool_omer@hotmail.co.uk>
2021-04-06 01:49:33 +0500

    password verification

--- finger_print_authentication.ino
+++ finger_print_authentication.ino
@@ -1,5 +1,7 @@
+#include <SoftwareSerial.h>
+
 // default module password and address. each 4 bytes
-#define M_PASSWORD 0xFFFFFFFF
+#define M_PASSWORD 0x00000000
 #define M_ADDRESS 0xFFFFFFFF
 
 // packet IDs. (High byte transferred first)
@@ -19,15 +21,19 @@
 // default timeout to wait for response from scanner
 #define DEFAULT_TIMEOUT 2000
 
+SoftwareSerial sensorSerial(2, 3); // rx, tx -> yellow, white
+Stream *commSerial = &sensorSerial;
+
 // function to send packet to finger scanner
 bool sendPacket(uint8_t pid, uint8_t cmd, uint8_t* data, uint16_t data_length){
+  
   // seperate header and address into bytes
   uint8_t header_bytes[2];
   uint8_t address_bytes[4];
 
   header_bytes[0] = HEADER & 0xFF; // low byte
   header_bytes[1] = (HEADER >> 8) & 0xFF; // high byte
-
+  
   address_bytes[0] = M_ADDRESS & 0xFF; // low byte
   address_bytes[1] = (M_ADDRESS >> 8) & 0xFF;
   address_bytes[2] = (M_ADDRESS >> 16) & 0xFF;
@@ -57,29 +63,52 @@ bool sendPacket(uint8_t pid, uint8_t cmd, uint8_t* data, uint16_t data_length){
   check_sum_in_bytes[0] = check_sum & 0xFF; // get low byte
   check_sum_in_bytes[1] = (check_sum >> 8) & 0xFF; // get high byte
 
+//  Serial.println("writing");
   // write to serial (tx). high bytes will be transferred first
   // header
-  Serial.write(header_bytes[1]);
-  Serial.write(header_bytes[0]);
+  commSerial->write(header_bytes[1]);
+  commSerial->write(header_bytes[0]);
+  
+//  Serial.print(header_bytes[1], HEX);
+//  Serial.println(header_bytes[0], HEX);
   // address
-  Serial.write(address_bytes[3]);
-  Serial.write(address_bytes[2]);
-  Serial.write(address_bytes[1]);
-  Serial.write(address_bytes[0]);
+  commSerial->write(address_bytes[3]);
+  commSerial->write(address_bytes[2]);
+  commSerial->write(address_bytes[1]);
+  commSerial->write(address_bytes[0]);
+  
+//  Serial.print(address_bytes[3], HEX);
+//  Serial.print(address_bytes[2], HEX);
+//  Serial.print(address_bytes[1], HEX);
+//  Serial.println(address_bytes[0], HEX);
   // packet id
-  Serial.write(pid);
+  commSerial->write(pid);
+  
+//  Serial.println(pid, HEX);
   // packet length
-  Serial.write(packet_length_in_bytes[1]);
-  Serial.write(packet_length_in_bytes[0]);
+  commSerial->write(packet_length_in_bytes[1]);
+  commSerial->write(packet_length_in_bytes[0]);
+  
+//  Serial.print(packet_length_in_bytes[1], HEX);
+//  Serial.println(packet_length_in_bytes[0], HEX);
   // command
-  Serial.write(cmd);
+  commSerial->write(cmd);
+  
+//  Serial.println(cmd, HEX);
   // data
   for(int i = (data_length - 1); i >= 0; i--){
-    Serial.write(data[i]);
+    commSerial->write(data[i]);
+//    Serial.print(data[i], HEX);
   }
+//  Serial.println();
   // checksum
-  Serial.write(check_sum_in_bytes[1]);
-  Serial.write(check_sum_in_bytes[0]);
+  commSerial->write(check_sum_in_bytes[1]);
+  commSerial->write(check_sum_in_bytes[0]);
+  
+//  Serial.print(check_sum_in_bytes[1], HEX);
+//  Serial.println(check_sum_in_bytes[0], HEX);
+//  
+//  Serial.println("written");
 
   return true;
 }
@@ -94,8 +123,9 @@ bool receivePacket(uint32_t timeout = DEFAULT_TIMEOUT){
 
   // wait for response for set timout
   while(timeout > 0){
-    if(Serial.available()){
-      byte_buffer = Serial.read();
+    if(commSerial->available()){
+      byte_buffer = commSerial->read();
+//      Serial.println(byte_buffer, HEX);
 
       serial_buffer[serial_buffer_length] = byte_buffer;
       serial_buffer_length++;
@@ -106,12 +136,12 @@ bool receivePacket(uint32_t timeout = DEFAULT_TIMEOUT){
   }
 
   if(serial_buffer_length == 0){ // received nothing
-//    Serial.println("received nothing\n");
+    Serial.println("received nothing\n");
     return false;
   }
 
   if(serial_buffer_length < 10){ // bad packet
-//    Serial.print("bad packet nigga\n");
+    Serial.print("bad packet nigga\n");
     return false;
   }
 
@@ -130,30 +160,48 @@ bool receivePacket(uint32_t timeout = DEFAULT_TIMEOUT){
       // case 0 & 1 check for start code
       case 0:
         // high byte
-        if(serial_buffer[token] == ((HEADER >> 8) & 0xFF)) break;
-        else return false;
+        if(serial_buffer[token] == ((HEADER >> 8) & 0xFF)){
+          break;
+        }
+        
+        return false;
         
       case 1:
         // low byte
-        if(serial_buffer[token] == (HEADER & 0xFF)) break;
-        else return false;
+        if(serial_buffer[token] == (HEADER & 0xFF)){
+          break;
+        }
+        
+        return false;
         
       // cases 2 to 5 check for device address. high to low byte
       case 2:
-        if(serial_buffer[token] == ((M_ADDRESS >> 24) & 0xFF) ) break;
-        else return false;
+        if(serial_buffer[token] == ((M_ADDRESS >> 24) & 0xFF) ){
+          break;
+        }
+        
+        return false;
 
       case 3:
-        if(serial_buffer[token] == ((M_ADDRESS >> 16) & 0xFF) ) break;
-        else return false;
+        if(serial_buffer[token] == ((M_ADDRESS >> 16) & 0xFF) ){
+          break;
+        }
+        
+        return false;
 
       case 4:
-        if(serial_buffer[token] == ((M_ADDRESS >> 8) & 0xFF) ) break;
-        else return false;
+        if(serial_buffer[token] == ((M_ADDRESS >> 8) & 0xFF) ) {
+          break;
+        }
+        
+        return false;
 
       case 5:
-        if(serial_buffer[token] == (M_ADDRESS & 0xFF) ) break;
-        else return false;
+        if(serial_buffer[token] == (M_ADDRESS & 0xFF) ) {
+          break;
+        }
+        
+        return false;
 
       // check for valid packet type {refactor to exclude command id}
       case 6:
@@ -161,6 +209,7 @@ bool receivePacket(uint32_t timeout = DEFAULT_TIMEOUT){
           packet_type = serial_buffer[token];
           break;
         }
+        
         return false;
 
       // read packet data length
@@ -170,10 +219,12 @@ bool receivePacket(uint32_t timeout = DEFAULT_TIMEOUT){
           rx_packet_length[1] = serial_buffer[token]; // high byte
           rx_packet_length_2b = uint16_t(rx_packet_length[1] << 8) + rx_packet_length[0]; // the full length
           data_length = rx_packet_length_2b - 3; // 2 checksum , 1 conformation code
-
+          
           token ++;
+
           break;
         }
+        
         return false;
 
       // case 8 won't be hit as after case 7, token value is 9
@@ -185,7 +236,7 @@ bool receivePacket(uint32_t timeout = DEFAULT_TIMEOUT){
       // read data
       case 10:
         for(int i = 0; i < data_length; i++){
-          data_buffer[(data_buffer_length - 1) - i] = serial_buffer[token + i]; // low bytes will be written at end of array. Inherently, high bytes appear first
+          data_buffer[(data_length - 1) - i] = serial_buffer[token + i]; // low bytes will be written at end of array. Inherently, high bytes appear first
         }
         break;
 
@@ -202,8 +253,12 @@ bool receivePacket(uint32_t timeout = DEFAULT_TIMEOUT){
           tmp = packet_type + rx_packet_length[0] + rx_packet_length[1] + confirmation_code;
 
           // if calculated checksum is equal to received checksum. is good to go
-          if(tmp == checksum) return true;
-          else return false;
+          if(tmp == checksum){
+//          Serial.println("pass");
+            return true;
+          }
+//          Serial.println("fail");
+          return false;
 
           break;
         }
@@ -222,16 +277,24 @@ bool receivePacket(uint32_t timeout = DEFAULT_TIMEOUT){
             tmp += data_buffer[i]; //calculate data checksum
           }
 
-          if(tmp == checksum) return true;
+          if(tmp == checksum) {
+//            Serial.println("pass");
+            return true;
+          }
+//          Serial.println("fail");
           return false;
 
           break;
         }
 
         // if checksum is 0
-        else return false;
+        else {
+//          Serial.println("fail");
+          return false;
+        }
 
       default:
+      Serial.println("default");
         break;
     }
 
@@ -243,26 +306,32 @@ bool verifyPassword(uint32_t password = M_PASSWORD){
   // store password seperately in 4 bytes. isn't a necessity
   uint8_t password_bytes[4] = {0};
   password_bytes[0] = password & 0xFF;
-  password_bytes[1] = (password >> 8) & 0xFF;
-  password_bytes[2] = (password >> 16) & 0xFF;
-  password_bytes[3] = (password >> 24) & 0xFF;
+  password_bytes[1] = (password >> 8);
+  password_bytes[2] = (password >> 16);
+  password_bytes[3] = (password >> 24);
 
-  bool response = sendPacket(PID_COMMAND, CMD_VERIFY_PASSWORD, password_bytes, 4);
 
-  return response;
+  bool tx_response = sendPacket(PID_COMMAND, CMD_VERIFY_PASSWORD, password_bytes, 4);
+  bool rx_response = receivePacket();
+  
+  if(rx_response){
+    return true;
+  }
+  return false;
 }
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
+  sensorSerial.begin(57600);
 
   bool response = verifyPassword();
+
+  if (response) Serial.println("Password Verified");
+  else Serial.println("Password Unverified");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Serial.available() > 0){
-
-  }
 }
